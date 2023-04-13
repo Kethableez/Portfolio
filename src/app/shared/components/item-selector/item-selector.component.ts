e2de2ca2d41e7ad32613fc8d333acbc6c5536dec
Kethableez <janiszyn.amadeusz@gmail.com>
2023-04-13 17:39:42 +0200

    Animations

--- src/app/shared/components/item-selector/item-selector.component.ts
+++ src/app/shared/components/item-selector/item-selector.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import {
   Component,
   EventEmitter,
@@ -16,6 +17,29 @@ export interface Value<T> {
   selector: 'ktbz-item-selector',
   templateUrl: './item-selector.component.html',
   styleUrls: ['./item-selector.component.scss'],
+  animations: [
+    trigger('selectorAnim', [
+      transition('void => *', [
+        style({
+          height: 0,
+          opacity: 0,
+          transform: 'scale(0)',
+        }),
+        animate(
+          100,
+          style({
+            height: '*',
+            opacity: '*',
+          })
+        ),
+        animate(100, style({ transform: 'scale(1)' })),
+      ]),
+      transition('* => void', [
+        animate(100, style({ transform: 'scale(0)' })),
+        animate(100, style({ opacity: 0, height: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class ItemSelectorComponent implements OnInit {
   @Input()
@@ -24,29 +48,42 @@ export class ItemSelectorComponent implements OnInit {
   @Output()
   onSelectedValue = new EventEmitter<Value<string>>();
 
-  @HostListener('wheel', ['$event'])
-  onScroll(event: WheelEvent) {
-    if (event.deltaY === 100) {
-      const nextItem =
-        this.selectedIndex === this.items.length - 1
-          ? this.items[this.selectedIndex]
-          : this.items[this.selectedIndex + 1];
-      this.select(nextItem);
-    } else {
-      const nextItem =
-        this.selectedIndex === 0
-          ? this.items[this.selectedIndex]
-          : this.items[this.selectedIndex - 1];
-      this.select(nextItem);
-    }
-  }
+  // @HostListener('wheel', ['$event'])
+  // onScroll(event: WheelEvent) {
+  //   if (event.deltaY === 100) {
+  //     const nextItem =
+  //       this.selectedIndex === this.items.length - 1
+  //         ? this.items[this.selectedIndex]
+  //         : this.items[this.selectedIndex + 1];
+  //     this.select(nextItem);
+  //   } else {
+  //     const nextItem =
+  //       this.selectedIndex === 0
+  //         ? this.items[this.selectedIndex]
+  //         : this.items[this.selectedIndex - 1];
+  //     this.select(nextItem);
+  //   }
+  // }
+
+  displayItems: Value<string>[] = [];
 
   selected!: Value<string>;
 
   constructor() {}
 
   ngOnInit(): void {
+    this.displayItems = this.items.slice(0, 4);
     this.selected = this.items[0];
+    // console.log(this.displayItems);
+    // this.selected = this.items[0];
+  }
+
+  sel(item: Value<string>) {
+    const idx = this.items.findIndex((i) => i.value === item.value);
+    const from = idx - 3;
+    const to = idx + 4;
+    this.displayItems = this.items.slice(from < 0 ? 0 : from, to);
+    this.selected = item;
   }
 
   getClass(item: Value<string>) {
--- src/app/shared/components/scrollable-content/scrollable-content.component.ts
+++ src/app/shared/components/scrollable-content/scrollable-content.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { RichContentData } from '../rich-content/rich-content.component';
 
@@ -5,6 +6,37 @@ import { RichContentData } from '../rich-content/rich-content.component';
   selector: 'ktbz-scrollable-content',
   templateUrl: './scrollable-content.component.html',
   styleUrls: ['./scrollable-content.component.scss'],
+  animations: [
+    trigger('contentAnim', [
+      transition('void => *', [
+        style({
+          height: 0,
+          width: 0,
+          opacity: 0,
+          'overflow-y': 'hidden',
+        }),
+        animate(
+          '100ms',
+          style({
+            height: '*',
+            width: '*',
+          })
+        ),
+        animate(100, style({ opacity: '*' })),
+        animate(100),
+      ]),
+      transition('* => void', [
+        animate(50, style({ opacity: 0, height: 0 })),
+        animate(
+          200,
+          style({
+            'overflow-y': 'hidden',
+            width: 0,
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class ScrollableContentComponent implements OnInit {
   @Input()
@@ -15,6 +47,9 @@ export class ScrollableContentComponent implements OnInit {
   currentIndex = 0;
 
   constructor() {}
+  // 8 elem
+  // 7 last index
+  // 5, 6, 7 - last elems
 
   ngOnInit(): void {
     this.data = this.scrollableData.slice(
@@ -27,56 +62,92 @@ export class ScrollableContentComponent implements OnInit {
   next() {
     if (this.currentIndex === this.scrollableData.length - 1) return;
     this.currentIndex++;
-    if (this.currentIndex === 0) {
-      this.data = this.scrollableData.slice(0, 3);
-    } else if (this.currentIndex === this.scrollableData.length - 1) {
-      this.data = this.scrollableData.slice(-3);
-    } else {
+    if (this.currentIndex < this.scrollableData.length - 2) {
       this.data = this.scrollableData.slice(
         this.currentIndex - 1,
         this.currentIndex + 2
       );
-    }
-  }
-
-  getClass(index: number) {
-    if (this.currentIndex === 0) {
-      return index === this.currentIndex
-        ? ''
-        : index === 1
-        ? 'dimmed-20'
-        : index === 2
-        ? 'dimmed-40'
-        : 'hidden';
-    } else if (this.currentIndex === this.scrollableData.length - 1) {
-      return index === this.currentIndex
-        ? ''
-        : index === this.scrollableData.length - 2
-        ? 'dimmed-20'
-        : index === this.scrollableData.length - 3
-        ? 'dimmed-40'
-        : 'hidden';
     } else {
-      return index === this.currentIndex
-        ? ''
-        : Math.abs(this.currentIndex - index) === 1
-        ? 'dimmed-20'
-        : 'hidden';
+      this.data = this.scrollableData.slice(this.scrollableData.length - 3);
     }
   }
 
   prev() {
     if (this.currentIndex === 0) return;
     this.currentIndex--;
-    if (this.currentIndex === 0) {
-      this.data = this.scrollableData.slice(0, 3);
-    } else if (this.currentIndex === this.scrollableData.length - 1) {
-      this.data = this.scrollableData.slice(-3);
-    } else {
+    if (this.currentIndex > 1) {
       this.data = this.scrollableData.slice(
         this.currentIndex - 1,
         this.currentIndex + 2
       );
+    } else {
+      this.data = this.scrollableData.slice(0, 3);
     }
   }
+
+  getStyle(index: number) {
+    if (this.currentIndex === 0) {
+      return index === 0 ? '' : index === 1 ? 'dimmed-20' : 'dimmed-40';
+    } else if (this.currentIndex === this.scrollableData.length - 1) {
+      return index === 0 ? 'dimmed-40' : index === 1 ? 'dimmed-20' : '';
+    } else {
+      return index === 1 ? '' : 'dimmed-20';
+    }
+  }
+
+  // next() {
+  //   if (this.currentIndex === this.scrollableData.length - 1) return;
+  //   this.currentIndex++;
+  //   if (this.currentIndex === 0) {
+  //     this.data = this.scrollableData.slice(0, 3);
+  //   } else if (this.currentIndex === this.scrollableData.length - 1) {
+  //     this.data = this.scrollableData.slice(-3);
+  //   } else {
+  //     this.data = this.scrollableData.slice(
+  //       this.currentIndex - 1,
+  //       this.currentIndex + 2
+  //     );
+  //   }
+  // }
+
+  // getClass(index: number) {
+  //   if (this.currentIndex === 0) {
+  //     return index === this.currentIndex
+  //       ? ''
+  //       : index === 1
+  //       ? 'dimmed-20'
+  //       : index === 2
+  //       ? 'dimmed-40'
+  //       : 'hidden';
+  //   } else if (this.currentIndex === this.scrollableData.length - 1) {
+  //     return index === this.currentIndex
+  //       ? ''
+  //       : index === this.scrollableData.length - 2
+  //       ? 'dimmed-20'
+  //       : index === this.scrollableData.length - 3
+  //       ? 'dimmed-40'
+  //       : 'hidden';
+  //   } else {
+  //     return index === this.currentIndex
+  //       ? ''
+  //       : Math.abs(this.currentIndex - index) === 1
+  //       ? 'dimmed-20'
+  //       : 'hidden';
+  //   }
+  // }
+
+  // prev() {
+  //   if (this.currentIndex === 0) return;
+  //   this.currentIndex--;
+  //   if (this.currentIndex === 0) {
+  //     this.data = this.scrollableData.slice(0, 3);
+  //   } else if (this.currentIndex === this.scrollableData.length - 1) {
+  //     this.data = this.scrollableData.slice(-3);
+  //   } else {
+  //     this.data = this.scrollableData.slice(
+  //       this.currentIndex - 1,
+  //       this.currentIndex + 2
+  //     );
+  //   }
+  // }
 }
